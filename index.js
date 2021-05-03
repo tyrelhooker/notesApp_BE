2a18817bbac8df3c8d5b5cb879bcdc048893b8a5
TJ Hooker <tyrelhooker@gmail.com>
2021-05-02 23:15:39 -0400

    Add eslint

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-const db = require("./db.json");
 const morgan = require('morgan');
 const cors = require('cors');
 const Note = require('./models/note');
 const { response } = require('express');
 
-
-
 const app = express();
 
 // MIDDLEWARE
 app.use(express.static('build'));
+app.use(cors());
 app.use(express.json());
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
 morgan.token('reqBody', (req, res) => JSON.stringify(req.body))
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'));
-
-
 
 // ROUTES
 app.get('/api/notes', (req, res) => {
@@ -25,18 +32,6 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 })
-
-// app.get('/info', (req, res) => {
-//   const totalnotes = notes.length;
-//   console.log(totalnotes);
-//   // console.log(notes.length)
-//   const calcDate = new Date();
-  
-//   res.send(
-//     `<p>Notebook has info for ${totalnotes} people.</p>
-//     <p>${calcDate}</p>`
-//   )
-// })
 
 app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
@@ -47,30 +42,19 @@ app.get('/api/notes/:id', (req, res, next) => {
         res.status(404).end();
       }
     })
-    // .catch(error => {
-    //   console.log(error);
-    //   res.status(400).send({ error: 'malformatted id' });
-    // })
     .catch(error => next(error));
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(result => {
+  Note.findByIdAndDelete(req.params.id)
+    .then(res => {
       res.status(204).end();
     })
     .catch(error => next(error));
-//   const id = Number(req.params.id);
-//   const newnotes = notes.filter(note => note.id !== id);
-//   notes = newnotes;
-//   console.log(notes);
-
-//   res.status(404).send('note deleted').end();
 })
 
 app.post('/api/notes', (req, res) => {
   const body = req.body;
-  // console.log(body);
 
   if(!body.content) {
     return res.status(400).json({ error: 'content missing'})
@@ -113,21 +97,12 @@ app.put('/api/notes/:id', (req, res, next) => {
 })
 
 // MIDDLEWARE - MUST BE AFTER THE ROUTES AND LAST MIDDLEWARE
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' });
 }
 
 app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id'})
-  }
-
-  next(error)
-}
 
 app.use(errorHandler);
 
